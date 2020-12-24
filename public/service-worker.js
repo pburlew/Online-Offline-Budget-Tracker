974a771d4ef1903fd6fdec97c4efbbf6e1d58530
Paige Burlew <paigeburlew@gmail.com>
2020-12-23 18:18:45 -0600

    added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+  ];
+
+
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+
+  //self.addEventListener
+
+
+  //self.skipWaiting
+
+  
+  //self.addEventListener
+
+
+  //fetch
+
+  //self.addEventListener fetch function
+
+  
