330913fc71b33d4a516165189fe3c7dc6ef43b9f
Paige Burlew <paigeburlew@gmail.com>
2021-01-11 21:18:59 -0600

    sw done

--- public/service-worker.js
+++ public/service-worker.js
@@ -32,9 +32,56 @@ const FILES_TO_CACHE = [
   
   //self.addEventListener
 
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+
 
   //fetch
 
   //self.addEventListener fetch function
 
+  self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
   
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+         caches.match(evt.request).then(response => {
+           console.log(response);
+          return response || fetch(evt.request);
+        })
+      
+    );
+  });
